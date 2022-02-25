9e401c22464926c995b65a899b8263745126bdc9
Anh Nguyet Vu <anngvu@gmail.com>
2022-02-24 18:03:38 -0700

    Better error handling for update job

--- update_portal_tables/update_portal_tables.R
+++ update_portal_tables/update_portal_tables.R
@@ -3,6 +3,7 @@ reticulate::use_virtualenv("/r-reticulate")
 library(nfportalutils)
 library(httr)
 
+# Values -----------------------------------------------------------------------#
 # Extract and use token
 secrets <- jsonlite::fromJSON(Sys.getenv("SCHEDULED_JOB_SECRETS"))
 syn_login(authtoken = secrets[["SYNAPSE_AUTH_TOKEN"]])
@@ -17,66 +18,73 @@ job <- list(
   subjob2 = paste(schedule, "update_study_annotations"), # (target table: portal_files)
   subjob3 = paste(schedule, "assign_study_data_types") #  (target table: portal_studies)
 )
-
 # Store job outputs to transmit to notification system 
 report <- lapply(job, function(x) paste(":white_check_mark:", x))
+
+# Helper funs ------------------------------------------------------------------#
+
+# Optional functionality to send slack report
+# report = a list of messages
+slack_report <- function(report) {
+  if(Sys.getenv("SLACK") != "") {
+    report <- list(text = paste(report, collapse = "\n"))
+    slack_hook <- Sys.getenv("SLACK")
+    report <- jsonlite::toJSON(report, auto_unbox = TRUE)
+    post_status <- httr::POST(url = slack_hook, body = report, content_type_json())
+    cat(post_status$status_code)
+  } 
+}
 
 # The final error or message output will replace default report status 
 handleError <- function(e, subjob) {
   report[[subjob]] <<- paste(":x:", job[[subjob]], " failed!")
-  traceback() # for the person who has to look at the internal logs
+  traceback()
 }
 
 handleMessage <- function(m, subjob) {
   report[[subjob]] <<- paste(":white_check_mark:", job[[subjob]], "- completed with message:", m$message)
 }
 
-# Subjob 1: Update related studies column
-withCallingHandlers(
-  {
-    calculate_related_studies(study_tab_id, n_clust = 30, dry_run = FALSE)
+# Main -------------------------------------------------------------------------#
+
+tryCatch({
+  # Subjob 1: Update related studies column
+    withCallingHandlers(
+    {
+      calculate_related_studies(study_tab_id, n_clust = 30, dry_run = FALSE)
     }, 
-  # message = function(m) handleMessage(m, "subjob1"), # no useful messages currently
-  error = function(e) handleError(e, "subjob1")
-)
-
-# Subjob 2: Update file annotations that can be derived from study table
-withCallingHandlers(
-  {
-    update_study_annotations(study_table_id = study_tab_id,
-                             fileview_id = portal_fileview_id,
-                             annotations = c("studyId","studyName","fundingAgency","initiative"),
-                             dry_run = FALSE)
-  }, 
-  message = function(m) handleMessage(m, "subjob2"),
-  error = function(e) handleError(e, "subjob2")
-)
-
-# Subjob 3: Update study "data type" summary values
-withCallingHandlers(
-  {
-    data_types <- get_valid_values_from_json_schema("https://raw.githubusercontent.com/nf-osi/nf-metadata-dictionary/main/NF.jsonld")
-    assign_study_data_types(study_table_id = study_tab_id,
-                            fileview_id = portal_fileview_id,
-                            valid_values = data_types,
-                            dry_run = FALSE)
+    # message = function(m) handleMessage(m, "subjob1"), # no useful messages currently
+    error = function(e) handleError(e, "subjob1")
+  )
+  
+  # Subjob 2: Update file annotations that can be derived from study table
+  withCallingHandlers(
+    {
+      update_study_annotations(study_table_id = study_tab_id,
+                               fileview_id = portal_fileview_id,
+                               annotations = c("studyId","studyName","fundingAgency","initiative"),
+                               dry_run = FALSE)
     }, 
-  # message = function(m) handleMessage(m, "subjob3"), # no useful messages currently
-  error = function(e) handleError(e, "subjob3")
+    message = function(m) handleMessage(m, "subjob2"),
+    error = function(e) handleError(e, "subjob2")
+  )
+  
+  # Subjob 3: Update study "data type" summary values
+  withCallingHandlers(
+    {
+      data_types <- get_valid_values_from_json_schema("https://raw.githubusercontent.com/nf-osi/nf-metadata-dictionary/main/NF.jsonld")
+      assign_study_data_types(study_table_id = study_tab_id,
+                              fileview_id = portal_fileview_id,
+                              valid_values = data_types,
+                              dry_run = FALSE)
+    }, 
+    # message = function(m) handleMessage(m, "subjob3"), # no useful messages currently
+    error = function(e) handleError(e, "subjob3")
+  )},
+  
+  finally = slack_report(report)
 )
 
-# Summary digest
-report <- list(text = paste(report, collapse = "\n"))
-print(report)
-
-# If configured, also send externally
-if(Sys.getenv("SLACK") != "") {
-  # Send summary notification 
-  slack_hook <- Sys.getenv("SLACK")
-  report <- jsonlite::toJSON(report, auto_unbox = TRUE)
-  post_status <- httr::POST(url = slack_hook, body = report, content_type_json())
-  cat(post_status$status_code)
-} 
 
 
 
