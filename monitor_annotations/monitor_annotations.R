ceeda9200a5014798a8d76bb65ac2ce8b6c3e239
Anh Nguyet Vu <anngvu@gmail.com>
2022-08-11 18:11:36 -0600

    Update with dry run

--- monitor_annotations/monitor_annotations.R
+++ monitor_annotations/monitor_annotations.R
@@ -24,11 +24,11 @@ source("https://raw.githubusercontent.com/nf-osi/jobs/764c62a94be9fd4dc2b0e04dd3
 # Filter for non-annotated files
 #' @param dt Data.table with props `name`, `createdBy`, `resourceType`
 #' @param ignore_file File pattern to ignore.
-#' @param ignore_user List of user ids to ignore. Defaults to NF service accounts.
+#' @param ignore_user List of user ids to ignore. Defaults to NF service accounts. 
 #' @param list_len Max length of list of files to return for each user. Default 50.
 processNA <- function(dt,
                      ignore_file = "data sharing plan|synapse_storage_manifest|progress report",
-                     ignore_user = c(3421893, 3423450), # nf-osi-service, nf-bot
+                     ignore_user = c(3421893, 3423450), # nf-osi-service, nf-bot 
                      list_len = 50
                      ) {
   
@@ -36,8 +36,8 @@ processNA <- function(dt,
   dt <- dt[!grepl(ignore, name, ignore.case = TRUE)][!createdBy %in% ignore_user]
   n <- dt[is.na(resourceType), .N]
   # Assemble list of creator ~ files for clear list of assignments
-  assignments <- split(dt[is.na(resourceType), .(files = first(name, list_len)), by = createdBy], 
-                       by = "createdBy", keep.by = F)
+  assignments <- split(dt[is.na(resourceType)], by = "createdBy", keep.by = F)
+  assignments <- sapply(assignments, function(x) paste0(head(x$name, list_len), " (", head(x$id, list_len), ")"))
   return(list(n = n, assignments = assignments))
 }
 
@@ -53,37 +53,46 @@ checkNA <- function(files) {
 #' @param user One or more synapse user ids
 #' @param files A representative list of files assigned to the user to include in message.
 #' @param project Optional project name or id for context (goes into message subject).
-#' @param dcc The DCC Synapse profile id (numeric) to cc on the message. Use FALSE to not copy the DCC.   
-emailReAnnotation <- function(user, 
-                               files,
-                               project = NULL, 
-                               dcc = FALSE
-                                ) {
+#' @param dcc The DCC Synapse profile id (numeric) to cc on the message. Use FALSE to not copy the DCC.  
+#' @param dry_run If TRUE, output message instead of emailing. 
+emailReAnnotation <- function(user,
+                              files,
+                              project = NULL, 
+                              dcc = 3421893,
+                              dry_run = TRUE
+                              ) {
   msg_template <- 
   "Dear contributor,
   
-  Our system shows you have uploaded files which are presently unannotated (see some files apppended below for reference). 
+  Our system shows you have uploaded files which are presently unannotated (some files appended below for reference). 
   NF-OSI encourages that data files be annotated according to the docs here: https://help.nf.synapse.org/NFdocs/how-to-annotate-data.
   You can ignore this reminder for any non-data files such as analyses and presentations.
-  If you think you have received this message in error, please contact nf-osi@sagebionetworks.org.
+  If you believe you have received this message in error, please contact nf-osi@sagebionetworks.org.
   
   Thank you,
   NF-OSI Service"
   
-  msg_append <- paste(files, collapse = "\n")
+  msg_listing <- paste(files, collapse = "\n")
   
   msg_body <- paste0(msg_template,
                      "\n\n-----\n\n",
-                     msg_append)
+                     msg_listing,
+                     "\n\n-----\n\n")
   
   msg_subj <- glue::glue("Please annotate files you've deposited for Synapse project {project}")
     
   recipients <- if(dcc) c(user, dcc) else user
   recipients <- as.character(recipients)
-  success <- .syn$sendMessage(userIds = recipients, 
-                              messageSubject = msg_subj, 
-                              messageBody = msg_body,
-                              contentType = "text/plain")
+  if(dry_run) {
+    cat("to:", recipients, "\n",
+        "subject:", msg_subj, "\n\n",
+        msg_body)
+  } else {
+    .syn$sendMessage(userIds = recipients,
+                     messageSubject = msg_subj, 
+                     messageBody = msg_body,
+                     contentType = "text/plain")
+  }
 }
 
 
@@ -95,7 +104,8 @@ try({
       
       for(fileview in dt$studyFileviewId) {
         # Issues to handle:
-        # 1) Fileviews can break and be unquery-able, the most common error something like: 'attribute X size is too small, needs to be __'
+        # 1) Fileviews can break and be unquery-able, the most common error something like: 
+        # 'attribute X size is too small, needs to be __'
         # 2) Columns in query for may be missing for some reason
         files[[fileview]] <- try(
           .syn$tableQuery(
@@ -113,7 +123,7 @@ try({
       assignments <- lapply(todo, function(x) x$assignments)
       for(project in names(assignments)) {
         for(user in names(assignments[[project]]) ) {
-          emailReAnnotation(user = user, files = assignments[[project]][[user]], project = project)
+          emailReAnnotation(user = user, files = assignments[[project]][[user]], project = project, dry_run = TRUE)
         }
       }
       
