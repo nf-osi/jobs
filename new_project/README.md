5fd38b66add391cd6ff002279e13f6d140215397
anngvu <32753274+anngvu@users.noreply.github.com>
2022-09-16 15:57:22 -0700

    Update new_project (#26)
    * Refactor with control loop
    
    * Update last step as write output
    
    * Update README
    
    * Add test file
    
    * Move test file
    
    * Update Dockerfile

--- new_project/README.md
+++ new_project/README.md
@@ -4,6 +4,6 @@ Create a new project according to the NF template.
 
 ### Example
 
-Use with FILE that is a JSON config.
-`docker run -v "$(pwd)":/app -e SYNAPSE_AUTH_TOKEN=$SYNAPSE_AUTH_TOKEN ghcr.io/nf-osi/jobs-new-project $FILE`
+Use with one or more JSON config files.
+`docker run -v "$(pwd)":/app -e SYNAPSE_AUTH_TOKEN=$SYNAPSE_AUTH_TOKEN ghcr.io/nf-osi/jobs-new-project config1.json config2.json`
 
--- new_project/new_project_from_json.R
+++ new_project/new_project_from_json.R
@@ -1,56 +1,66 @@
 #!/usr/bin/env Rscript
 
-config <- commandArgs(trailingOnly = TRUE)
+configs <- commandArgs(trailingOnly = TRUE)
 
 library(nfportalutils)
 syn_login()
 
-config <- jsonlite::read_json(config)
+setup_from_config <- function(config) {
+  
+  config <- jsonlite::read_json(config)
+  
+  NAME <- config$name
+  PI <- config$PI
+  LEAD <- config$dataLead
+  # Combined field for study table
+  LEADS <- paste(unique(c(PI, LEAD), sep = ",")) 
+  SUMMARY <- config$summary
+  FUNDER <- config$fundingAgency
+  INITIATIVE <- config$initiative
+  INSTITUTION <- paste(config$institution, sep = ";")
+  FOCUS <-  paste(config$diseaseFocus, sep = ",")
+  MANIFESTATIONS <- paste(config$diseaseManifestations, sep = ",")
+  GRANT_DOI <- paste(config$grantDOI, sep = ",")
+  
+  # Create
+  created_project <- new_project(name = NAME,
+                                 pi = PI,
+                                 lead = LEAD,
+                                 abstract = SUMMARY,
+                                 institution = INSTITUTION,
+                                 funder = FUNDER,
+                                 initiative = INITIATIVE)
+  
+  PROJECT_ID <- created_project$properties$id
+  FILEVIEW_ID <- attr(created_project, "fileview")
+  
+  # Register
+  STUDY_TABLE_ID <- if(Sys.getenv("PROFILE") == "TEST") "syn27353709" else "syn16787123"
+  nfportalutils::register_study(name = NAME,
+                                project_id = PROJECT_ID,
+                                abstract = SUMMARY, 
+                                lead = LEADS,
+                                institution = INSTITUTION, 
+                                focus = FOCUS, 
+                                manifestation = MANIFESTATIONS,
+                                fileview_id = FILEVIEW_ID,
+                                funder = FUNDER,
+                                initiative = INITIATIVE,
+                                grant_doi = GRANT_DOI,
+                                study_table_id = STUDY_TABLE_ID)
+  
+  # Add to scope of master portal fileview
+  nfportalutils::register_study_files(PROJECT_ID)
+  
+  # Write new syn id to config 
+  cat("Writing", PROJECT_ID, "to", config, "\n")
+  command <- paste0('jq \'. += { "studyId" : "',  PROJECT_ID, '" }\' ', config, ' > tmp.json && mv tmp.json ' , config)
+  system(command)
+}
 
-NAME <- config$name
-PI <- config$PI
-LEAD <- config$dataLead
-# Combined field for study table
-LEADS <- paste(unique(c(PI, LEAD), sep = ",")) 
-SUMMARY <- config$summary
-FUNDER <- config$fundingAgency
-INITIATIVE <- config$initiative
-INSTITUTION <- paste(config$institution, sep = ";")
-FOCUS <-  paste(config$diseaseFocus, sep = ",")
-MANIFESTATIONS <- paste(config$diseaseManifestations, sep = ",")
-GRANT_DOI <- paste(config$grantDOI, sep = ",")
-
-# Create
-created_project <- new_project(name = NAME,
-                               pi = PI,
-                               lead = LEAD,
-                               abstract = SUMMARY,
-                               institution = INSTITUTION,
-                               funder = FUNDER,
-                               initiative = INITIATIVE)
-
-PROJECT_ID <- created_project$properties$id
-FILEVIEW_ID <- attr(created_project, "fileview")
-
-# Register
-STUDY_TABLE_ID <- if(Sys.getenv("PROFILE") == "TEST") "syn27353709" else "syn16787123"
-nfportalutils::register_study(name = NAME,
-                              project_id = PROJECT_ID,
-                              abstract = SUMMARY, 
-                              lead = LEADS,
-                              institution = INSTITUTION, 
-                              focus = FOCUS, 
-                              manifestation = MANIFESTATIONS,
-                              fileview_id = FILEVIEW_ID,
-                              funder = FUNDER,
-                              initiative = INITIATIVE,
-                              grant_doi = GRANT_DOI,
-                              study_table_id = STUDY_TABLE_ID)
-
-# Add to scope of master portal fileview
-nfportalutils::register_study_files(PROJECT_ID)
-
-# Output variables
-cat("SYNAPSE_PROJECT_ID=",PROJECT_ID, "\n",
-    "SYNAPSE_FILEVIEW_ID=",FILEVIEW_ID, 
-    file = "new_project.log", sep = "") 
+for(config in configs) {
+  cat("--- Preparing using", config, "---\n")
+  try({
+    setup_from_config(config)
+  })
+}
